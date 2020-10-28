e7ba22fed58dc35189e8648a2938a7acd495d202
ayuan <ayuan2500@163.com>
2020-10-28 18:12:29 +0800

    项目搭建

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+/**
+ * @author ayuan
+ * @version 1.0.0
+ * @ClassName User
+ * @Description 描述
+ * @createTime 2020年10月28日 18:09:00
+ */
+public class User {
+    private String username;
+    private String password;
+}
